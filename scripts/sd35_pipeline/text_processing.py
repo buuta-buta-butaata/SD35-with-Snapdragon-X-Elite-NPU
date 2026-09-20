c6afd05be2c79dbb1414f85e36b7791428d6563b
buuta-buta-butaata <buuta.buta.butaata@gmail.com>
2026-09-20 14:03:45 +0900

    Add npy data for text_encoder_3(t5xxl) debugging

--- scripts/sd35_pipeline/text_processing.py
+++ scripts/sd35_pipeline/text_processing.py
@@ -60,12 +60,12 @@ class TextProcessing:
 
             if config.t5_cache == "load":
                 import tensors_io as tio
-                prompt_embeds_3 = tio.load(r"prompt_embeds_3.npy")
-                uncond_embeds_3 = tio.load(r"uncond_embeds_3.npy")
+                prompt_embeds_3 = tio.load(r"./debug_data/prompt_embeds_3.npy")
+                uncond_embeds_3 = tio.load(r"./debug_data/uncond_embeds_3.npy")
             elif config.t5_cache == "save":
                 import tensors_io as tio
-                tio.save(r"./", "prompt_embeds_3.npy", prompt_embeds_3)
-                tio.save(r"./", "uncond_embeds_3.npy", uncond_embeds_3)
+                tio.save(r"./debug_data", "prompt_embeds_3.npy", prompt_embeds_3)
+                tio.save(r"./debug_data", "uncond_embeds_3.npy", uncond_embeds_3)
 
         prompt_embeds = self.pad_for_sd35(prompt_embeds, prompt_embeds_3 * config.t5_strength)
         uncond_embeds = self.pad_for_sd35(uncond_embeds, uncond_embeds_3 * config.t5_strength)
